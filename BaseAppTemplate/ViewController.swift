fcdab17a0a46395b398c782c4c67105ebabb92fd
long.phan <long.phan@onemount.com>
2025-05-27 16:37:22 +0700

    first init empty

---
+++ BaseAppTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseAppTemplate
+//
+//  Created by LONGPHAN on 27/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
